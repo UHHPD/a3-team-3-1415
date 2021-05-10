b8d94e4aa117dc89f1c82850122d85500b7b4b01
Constanttp <>
2021-05-10 20:36:37 +0000

    Aufgabe 2a ist erledigt

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    return pow(mu,k)*exp(-mu)/tgamma(k+1);
+}
+
+double prob(std::vector<int> daten, double mu){
+  double produkt = 1;
+  for(int i = 0; i < daten.size() ; ++i){
+    produkt *= poisson(mu, daten[i] );
+  }
+  return produkt;
 }
 
 int main() {
     using namespace std;
 
 
-    ifstream fin("datensumme.txt");
+    ifstream fin1("datensumme.txt");
     int n_i;
+    std::vector<int> daten;
     for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
+        fin1 >> n_i;
+        daten.push_back(n_i);
     }
-    fin.close();
+    
+    std::cout << "Likelihood(3.11538): " << prob(daten,3.11538) << std::endl;
+    fin1.close();
 }
